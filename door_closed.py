9894fc8e7481e40a6a54b83f7268b78f222e6349
David Squire <ldsquire@gmail.com>
2019-11-30 20:00:09 -0600

    Added Door Routines

--- door_closed.py
+++ door_closed.py
@@ -1,1 +1,10 @@
-from gpiozero import Motor
+from gpiozero import Motor, Button
+from time import sleep
+
+doorstatus = Button(25)
+actuator = Motor(forward=17, backward=18)
+
+if doorstatus.is_pressed == 0:
+    actuator.backward()
+    sleep(3)
+    actuator.stop()
--- door_open.py
+++ door_open.py
@@ -1,1 +1,13 @@
-from gpiozero import Motor
+from gpiozero import Motor, Button
+from time import sleep
+
+doorstatus = Button(25)
+actuator = Motor(forward=17, backward=18)
+
+if doorstatus.is_pressed:
+    actuator.forward()
+    sleep(3)
+    actuator.stop()
+
+
+
